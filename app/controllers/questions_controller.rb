94d3a007993911491baa5ef775087421e80a54b9
Runidan <rusin.dn+github@gmail.com>
2022-09-15 20:25:42 +0500

    feat: after lesson3

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,26 @@
 class QuestionsController < ApplicationController
+
+  def show
+    @question = Question.find_by id: params[:id]
+  end
+  
+  def destroy
+    @question = Question.find_by id: params[:id]
+    @question.destroy
+    redirect_to questions_path
+  end
+  
   def edit
     @question = Question.find_by id: params[:id]
+  end
+
+  def update
+    @question = Question.find_by id: params[:id]
+    if @question.update questions_params
+      redirect_to questions_path
+    else
+      render :edit
+    end
   end
   
   def index
