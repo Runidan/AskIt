885766c581b815f3fb61c42e8eaaa5db809272c0
Runidan <rusin.dn+github@gmail.com>
2022-09-15 17:30:01 +0500

    feat: lesson3 19:10

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,28 @@
 class QuestionsController < ApplicationController
+  def edit
+    @question = Question.find_by id: params[:id]
+  end
+  
   def index
     @questions = Question.all
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new questions_params
+    if @question.save
+      redirect_to questions_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def questions_params
+    params.require(:question).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/questions', to: 'questions#index' 
-  
+  resources :questions, only: %i[index new edit create]
+
   root 'pages#index'
 end
